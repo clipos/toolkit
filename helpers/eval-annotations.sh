340fb48e4cbf589ee48149638577ef9414461862
Nicolas Godinho <nicolas.godinho@ssi.gouv.fr>
2018-10-15 18:56:31 +0200

    eval-annotations: reordering script logical blocks
    Avoid leaving at the bottom of the file the main content of the script
    (this script may grow larger and larger for each annotation we want to
    process with it).
    Restore the inline documentation on the top of the file.
    
    Change-Id: I669b15490e2199ef5a404cac3b4f36f24c284697

--- helpers/eval-annotations.sh
+++ helpers/eval-annotations.sh
@@ -4,6 +4,43 @@
 
 set -e -u -o pipefail
 
+# Each project in the manifest can have a set of annotations, for example to
+# fetch upstream updates:
+#
+#   <project name="src_external_linux" path="src/external/linux" >
+#     <annotation name="UPSTREAM_0_URL" value="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git" />
+#     <annotation name="UPSTREAM_0_REFSPECS" value="refs/heads/linux-4.16.y:refs/heads/upstream/stable-4.16 refs/tags/v*:refs/tags/v*" />
+#     <annotation name="UPSTREAM_1_URL" value="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git" />
+#     <annotation name="UPSTREAM_1_REFSPECS" value="refs/heads/master:refs/heads/upstream/master refs/tags/v*:refs/tags/v*" />
+#   </project>
+#
+# This configuration creates two new remotes (upstream0 and upstream1), fetch
+# their references according to the given refspec(s) (i.e., fetch the specified
+# refspecs and rename them locally according to the mapping the refspec
+# describe, see chapter "Git Internals - The Refspec" of the Git Book for more
+# details).
+
+main() {
+    if [[ -z "${REPO_PATH:-}" ]]; then
+        echo "ERROR: Missing environment variables" >&2
+        echo "This script must be run with: repo forall [projects...] -c $0 <command>" >&2
+        exit 1
+    fi
+
+    case "${1:-}" in
+        pull-upstream)
+            echo "[*] Starting to fetch upstream for ${REPO_PATH:-}"
+            eval_annotations pull_upstream "UPSTREAM_GIT" "UPSTREAM_REFS"
+            ;;
+        *)
+            echo "ERROR: Unknown command" >&2
+            echo "Available commands:" >&2
+            echo "- pull-upstream" >&2
+            exit 1
+            ;;
+    esac
+}
+
 eval_annotations() {
     local call
     local index
@@ -28,22 +65,6 @@ eval_annotations() {
     done
 }
 
-# Each project in the manifest can have a set of annotations, for example to
-# fetch upstream updates:
-#
-#   <project name="src_external_linux" path="src/external/linux" >
-#     <annotation name="UPSTREAM_0_URL" value="https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git" />
-#     <annotation name="UPSTREAM_0_REFSPECS" value="refs/heads/linux-4.16.y:refs/heads/upstream/stable-4.16 refs/tags/v*:refs/tags/v*" />
-#     <annotation name="UPSTREAM_1_URL" value="https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git" />
-#     <annotation name="UPSTREAM_1_REFSPECS" value="refs/heads/master:refs/heads/upstream/master refs/tags/v*:refs/tags/v*" />
-#   </project>
-#
-# This configuration creates two new remotes (upstream0 and upstream1), fetch
-# their references according to the given refspec(s) (i.e., fetch the specified
-# refspecs and rename them locally according to the mapping the refspec
-# describe, see chapter "Git Internals - The Refspec" of the Git Book for more
-# details).
-
 pull_upstream() {
     local index="$1"
     local git="$2"
@@ -65,23 +86,6 @@ pull_upstream() {
     echo
 }
 
-if [[ -z "${REPO_PATH:-}" ]]; then
-    echo "ERROR: Missing environment variables" >&2
-    echo "This script must be run with: repo forall [projects...] -c $0 <command>" >&2
-    exit 1
-fi
-
-case "${1:-}" in
-    pull-upstream)
-        echo "[*] Starting to fetch upstream for ${REPO_PATH:-}"
-        eval_annotations pull_upstream "UPSTREAM_GIT" "UPSTREAM_REFS"
-        ;;
-    *)
-        echo "ERROR: Unknown command" >&2
-        echo "Available commands:" >&2
-        echo "- pull-upstream" >&2
-        exit 1
-        ;;
-esac
+main "$@"
 
 # vim: set ts=4 sts=4 sw=4 et ft=sh:
